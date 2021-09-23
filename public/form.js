15752c6c67bdd5ce5500f9b31fb30d7f38853e19
astarchekova <77804854+astarchekova@users.noreply.github.com>
2021-09-23 11:35:26 +0100

    Menu

---
+++ public/form.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Form extends React.Component {
+    render(){
+        return (
+            <form> <input type="text" name="city" placeholder="город"/>
+            <button>Отправить форму</button>    
+
+            </form>
+        )
+    }
+}
